23f8563981fcd52ba7aff253bdea476cbd5e3865
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-10 15:59:55 +0300

    build controllers

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
 
 const app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
